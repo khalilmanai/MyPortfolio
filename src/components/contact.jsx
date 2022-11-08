6afae0f46d9943c662a2224eff124c4b640d233c
Khalil Manai <medkhalilmannai@gmail.com>
2022-11-08 19:48:47 +0100

    mailto fix

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <form method='POST' action='https://getform.io/f/80c5b892-1a13-4fa4-be51-4df14ab7a0e2' className='flex flex-col max-w-[600px] w-full'>
                 <div className='pb-8 mt-7'>
                     <p className='text-4xl font-bold inline border-b-4 border-[#FFB012] text-gray-300'>Contact</p>
-                    <p className='text-gray-300 py-4' > Sumbit the form below or get in touch with me on - myemail@gmail.com</p>
+                    <p className='text-gray-300 py-4' > Sumbit the form below or get in touch with me on: <strong className='text-[#FFB012]'><a href="mailto:medkhalilmannai@gmail.com">medkhalilmannai@gmail.com</a></strong></p>
                 </div>
                 <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
                 <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
