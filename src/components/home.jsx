0c820a6493366779ba319f5911fb231166da9fef
Khalil Manai <medkhalilmannai@gmail.com>
2022-11-08 18:29:35 +0100

    padding updates

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,16 +9,18 @@ const Home = () => {
 
       {/* Container */}
 
-      <div className='max-w-[1000px] mx-auto sm:py-0   sm: px-40 flex flex-col justify-center h-full'>
+      <div className='max-w-[1000px] mx-auto sm:py-0   sm: px-20 flex flex-col justify-center h-full'>
         <p className='text-[#FFB012]'> Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'> Khalil Manai</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'> I'm a Full Stack Developer. </h2>
-        <p className=' text-[#ccd6f6] py-4 max-w-[700px]'>
+        <div className='sm:min-w-[500px] max-w-[700px]'>
+        <p className=' text-[#ccd6f6] py-4 '>
           Highly self-motivated to consistently improve skills and grow professionally in tech stack , Experienced in
           UI/UX and Mobile Developement , interested in Decentralized systems and apps seeking to improve my
           knowledge and skills in with in-depth dive in the world of blockchain and various technologies
 
         </p>
+        </div>
         <div >
           <button className='group text-white border-2 flex  px-6  my-2 py-3  items-center hover:bg-[#FFB012] hover:border-[#FFB012] hover:space-x-3 duration-200'>
             <Link className='flex items-center ml-1' to="work" smooth={true} duration={800} >
